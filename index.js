141ac9d4eb75f98e767d94bb0c27084101419360
swapno92 <swapnoprograming92@gmail.com>
2023-10-20 16:36:28 +0600

    serversite to get and post my cart components data

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
     const database = client.db("brandShopBD");
     const brandsCollection = database.collection("brands");
     const productsCollection = database.collection("products");
+    const curtCollection = database.collection("curt");
 
     app.get("/brands", async (req, res) => {
       const brands = brandsCollection.find();
@@ -50,6 +51,48 @@ async function run() {
         _id: new ObjectId(id),
       });
       res.send(cursor);
+    });
+
+    app.post("/products", async (req, res) => {
+      const user = req.body;
+      const result = await productsCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateProduct = req.body;
+      console.log(updateProduct);
+      const product = {
+        $set: {
+          name: updateProduct.name,
+          brand: updateProduct.brand,
+          price: updateProduct.price,
+          rating: updateProduct.rating,
+          img: updateProduct.img,
+        },
+      };
+      const result = await productsCollection.updateOne(
+        filter,
+        product,
+        options
+      );
+      console.log(result);
+      res.send(result);
+    });
+
+    app.post("/curt", async (req, res) => {
+      const curt = req.body;
+      const result = await curtCollection.insertOne(curt);
+      res.send(result);
+    });
+
+    app.get("/curt", async (req, res) => {
+      const cursor = curtCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
